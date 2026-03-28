d656dad2ad08e5137e1d17c95c0ba8e8547f4ae0
anjanisolanki <anjanisolanki1220@gmail.com>
2026-03-28 14:08:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const modal = document.querySelector('.modal');
 const openBtn = document.getElementById('openModal');
 const closeBtn = document.querySelector('.close-modal');
 
+// Open the modal
 openBtn.onclick = function() {
     modal.style.display = "block";
 }
 
+// Close the modal with the 'X'
 closeBtn.onclick = function() {
     modal.style.display = "none";
 }
 
+// Close the modal by clicking the dark background
 window.onclick = function(event) {
     if (event.target === modal) {
         modal.style.display = "none";
